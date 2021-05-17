679557a1fba72200688ce3ab715f38c89b9840b2
Aswin <79461706+lofi2op@users.noreply.github.com>
2021-05-17 20:01:25 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,91 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+
+      //create sprites
+      car1 = createSprite(100,200);
+      car2 = createSprite(300,200);
+      car3 = createSprite(500,200);
+      car4 = createSprite(700,200);
+
+      cars = [car1,car2,car3,car4]
+
+
+
+    }
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+
+
+      var index = 0;
+      var x = 0;
+      var y;
+
+
+      //var display_position = 130;
+      for(var plr in allPlayers){
+    
+        //player1
+        //index = 1
+        //cars.x = x
+        //forget - 0
+        //cars[0]
+      
+      index += 1;
+      x += 200;
+      y = displayHeight - allPlayers[plr].distance;
+      cars[index-1].x = x;
+      cars[index-1].y = y;
+
+      if(index === player.index){
+        //fill("red");
+        cars[index-1].shapeColor = "red";
+        camera.position.x = displayWidth/2;
+        camera.position.y = cars[index-1].y;
+      }
+
+      
+      
+      
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+
+    drawSprites();
+  }
+}
